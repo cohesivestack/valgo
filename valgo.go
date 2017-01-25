3fffd89ad8ef629b667361cd4afed9656a622885
Carlos Forero <ecarlosforero@gmail.com>
2017-01-25 15:36:28 -0500

    Add Errors struct. Add Blank and NotBlank validation

--- valgo.go
+++ valgo.go
@@ -1,68 +1,97 @@
 package valgo
 
 import (
-  "fmt"
-  "strconv"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Validator struct {
-  value interface{}
-  errors []string
-  valid bool
+	value  interface{}
+	errors *Errors
+	valid  bool
 }
 
 func (this *Validator) Valid() bool {
-  return this.valid
+	return this.valid
 }
 
-func (this *Validator) Errors() []string {
-  return this.errors
+func (this *Validator) Errors() *Errors {
+	if this.errors.Messages != nil && len(this.errors.Messages) > 0 {
+		return this.errors
+	} else {
+		return nil
+	}
 }
 
-func (this *Validator) ValidAndErrors() (bool, []string) {
-  return this.valid, this.errors
+func (this *Validator) ValidAndErrors() (bool, *Errors) {
+	return this.valid, this.errors
 }
 
 func (this *Validator) Empty() *Validator {
-  value := this.ensureString()
-  if (len(value) > 0) {
-    this.valid = false
-    this.errors = append(this.errors, "Is not empty")
-  }
-  return this
+	value := this.ensureString()
+	if len(value) > 0 {
+		this.valid = false
+		this.errors.Add("Is not empty")
+	}
+	return this
 }
 
 func (this *Validator) NotEmpty() *Validator {
-  value := this.ensureString()
-  if (len(value) == 0) {
-    this.valid = false
-    this.errors = append(this.errors, "Is empty")
-  }
-  return this
+	value := this.ensureString()
+	if len(value) == 0 {
+		this.valid = false
+		this.errors.Add("Is empty")
+	}
+	return this
+}
+
+func (this *Validator) Blank() *Validator {
+	value := strings.Trim(this.ensureString(), " ")
+
+	if len(value) > 0 {
+		this.valid = false
+		this.errors.Add("Is not blank")
+	}
+	return this
+}
+
+func (this *Validator) NotBlank() *Validator {
+	value := strings.Trim(this.ensureString(), " ")
+
+	if len(value) == 0 {
+		this.valid = false
+		this.errors.Add("Is blank")
+	}
+	return this
 }
 
 func Is(value interface{}) *Validator {
-  validator := &Validator{
-    value: value,
-    errors: make([]string, 0, 0),
-    valid: true,
-  }
-  return validator
+	validator := &Validator{
+		value:  value,
+		errors: &Errors{Field: "field"},
+		valid:  true,
+	}
+	return validator
 }
 
+func (this *Validator) Called(name string) *Validator {
+	this.errors.Field = name
+	return this
+}
 
 func (this *Validator) ensureString() string {
-  switch v := this.value.(type) {
-  case uint8, uint16, uint32, uint64:
-    return strconv.FormatUint(this.value.(uint64), 10)
-  case int8, int16, int32, int64:
-    return strconv.FormatInt(this.value.(int64), 10)
-  case float32, float64:
-    return strconv.FormatFloat(this.value.(float64), 'f', -1, 64)
-  case string:
-    return this.value.(string)
-  default:
-    fmt.Printf("unexpected type %T", v)
-    return ""
-  }
+	switch v := this.value.(type) {
+	case uint8, uint16, uint32, uint64:
+		return strconv.FormatUint(this.value.(uint64), 10)
+	case int8, int16, int32, int64:
+		return strconv.FormatInt(this.value.(int64), 10)
+	case float32, float64:
+		return strconv.FormatFloat(this.value.(float64), 'f', -1, 64)
+	case string:
+		return this.value.(string)
+	default:
+		fmt.Printf("unexpected type %T", v)
+		return ""
+	}
 }
