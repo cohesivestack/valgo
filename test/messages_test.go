a9896a5c775c2ec4511d2629aa02992b52160a3e
Carlos Forero <ecarlosforero@gmail.com>
2018-01-18 20:12:10 -0500

    Add messages testing

--- test/messages_test.go
+++ test/messages_test.go
@@ -8,10 +8,13 @@ import (
 )
 
 func TestCustomMessageTemplate(t *testing.T) {
-	v := valgo.Is(" ").NotBlank("The field \"{{Title}}\" can't be empty")
-	assert.Contains(t, v.Errors()[0].Messages, "The field \"value0\" can't be empty")
+	v := valgo.Is(" ").NotBlank("The field \"{{Title}}\" can't be blank. :-)").Empty()
+	assert.Contains(t, v.Errors()[0].Messages, "The field \"value0\" can't be blank. :-)")
 
-	// Must not replace default blank message
+	// Should not replace the template for other validations
+	assert.Contains(t, v.Errors()[0].Messages, "\"value0\" must be empty")
+
+	// Should not replace default blank message
 	v = valgo.Is(" ").NotBlank()
 	assert.Contains(t, v.Errors()[0].Messages, "\"value0\" can't be blank")
 }
