4d0fa7a9cbe0170edfdc95f2a66f97a41e928825
Carlos Forero <ecarlosforero@gmail.com>
2018-02-01 23:28:15 -0500

    Add NotIdenticalTo validator

--- test/indentical_to_test.go
+++ test/indentical_to_test.go
@@ -66,3 +66,63 @@ func TestIdenticalToInvalid(t *testing.T) {
 	funcToTest(mapA, mapB)
 
 }
+
+func TestNotIdenticalToValid(t *testing.T) {
+	valgo.ResetMessages()
+
+	funcToTest := func(value1 interface{}, value2 interface{}) {
+		msg := fmt.Sprintf("not assert using %+v", value2)
+		v := valgo.Is(value1).NotIdenticalTo(value2)
+		assert.True(t, v.Valid(), msg)
+		assert.Empty(t, v.Errors(), msg)
+	}
+
+	for key, value := range map[interface{}]interface{}{
+		1:          "1",
+		"a":        "ab",
+		&[]int{10}: &[]int{10},
+		10.0:       10,
+	} {
+		funcToTest(key, value)
+	}
+
+	structA := MyStruct{
+		FieldInt: 1,
+	}
+	structB := structA
+
+	funcToTest(structA, structB)
+
+	arrayA := []int{10}
+	arrayB := arrayA
+	funcToTest(arrayA, arrayB)
+
+	mapA := map[string]int{"a": 1}
+	mapB := mapA
+	funcToTest(mapA, mapB)
+
+}
+
+func TestNotIdenticalToInvalid(t *testing.T) {
+	valgo.ResetMessages()
+
+	for _, value := range []interface{}{
+		10,
+		1,
+		"a",
+		"10",
+		&MyStruct{FieldInt: 10},
+		&[]int{10, 20},
+		&map[string]int{"a": 1}} {
+		msg := fmt.Sprintf("not assert using %+v", value)
+		v := valgo.Is(value).NotIdenticalTo(value)
+		assert.False(t, v.Valid(), msg)
+		if assert.NotEmpty(t, v.Errors(), msg) {
+			assert.Len(t, v.Errors(), 1, msg)
+			assert.Contains(t, v.Errors()[0].Messages,
+				fmt.Sprintf("\"value0\" can't be identical to \"%v\"", value),
+				msg)
+		}
+	}
+
+}
--- validator_indentical.go
+++ validator_indentical.go
@@ -4,19 +4,24 @@ import (
 	"reflect"
 )
 
-func (validator *Validator) IdenticalTo(value interface{}, template ...string) *Validator {
-
-	isComparableType := true
-
-	for _, v := range []interface{}{validator.currentValue, value} {
+func indenticalTo(valueA interface{}, valueB interface{}) bool {
+	for _, v := range []interface{}{valueA, valueB} {
 		_type := reflect.TypeOf(v)
 		switch _type.Kind() {
 		case reflect.Slice, reflect.Array, reflect.Map, reflect.Struct:
-			isComparableType = false
+			return false
 		}
 	}
 
-	if !isComparableType || validator.currentValue != value {
+	if valueA != valueB {
+		return false
+	}
+	return true
+}
+
+func (validator *Validator) IdenticalTo(value interface{}, template ...string) *Validator {
+
+	if !indenticalTo(validator.currentValue, value) {
 		validator.invalidate("identical_to",
 			map[string]interface{}{
 				"Title": validator.currentTitle,
@@ -26,7 +31,8 @@ func (validator *Validator) IdenticalTo(value interface{}, template ...string) *
 }
 
 func (validator *Validator) NotIdenticalTo(value interface{}, template ...string) *Validator {
-	if validator.currentValue == value {
+
+	if indenticalTo(validator.currentValue, value) {
 		validator.invalidate("not_identical_to",
 			map[string]interface{}{
 				"Title": validator.currentTitle,
