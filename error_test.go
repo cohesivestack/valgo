077d2deefa2fd111825f6236dac253c2a49db8c1
Carlos Forero <ecarlosforero@gmail.com>
2020-05-10 10:56:12 -0500

    Test Error JSON Marshal

--- error_test.go
+++ error_test.go
@@ -1,6 +1,7 @@
 package valgo
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -27,12 +28,15 @@ func TestAddErrorMessageFromValidator(t *testing.T) {
 
 	assert.False(t, v.Valid())
 	assert.Len(t, v.Errors(), 1)
+	assert.Len(t, v.Errors()["name"].Messages(), 1)
 	assert.Contains(t, v.Errors()["name"].Messages(), "Name must be blank")
 
 	v.AddErrorMessage("email", "Email is invalid")
 
 	assert.Len(t, v.Errors(), 2)
 	assert.False(t, v.Valid())
+	assert.Len(t, v.Errors()["name"].Messages(), 1)
+	assert.Len(t, v.Errors()["email"].Messages(), 1)
 	assert.Contains(t, v.Errors()["name"].Messages(), "Name must be blank")
 	assert.Contains(t, v.Errors()["email"].Messages(), "Email is invalid")
 }
@@ -44,12 +48,42 @@ func TestAddErrorMessageFromValgo(t *testing.T) {
 
 	assert.False(t, v.Valid())
 	assert.Len(t, v.Errors(), 1)
+	assert.Len(t, v.Errors()["email"].Messages(), 1)
 	assert.Contains(t, v.Errors()["email"].Messages(), "Email is invalid")
 
 	v.IsString("Vitalik Buterin", "name").Blank()
 
 	assert.Len(t, v.Errors(), 2)
 	assert.False(t, v.Valid())
+	assert.Len(t, v.Errors()["email"].Messages(), 1)
+	assert.Len(t, v.Errors()["name"].Messages(), 1)
 	assert.Contains(t, v.Errors()["email"].Messages(), "Email is invalid")
 	assert.Contains(t, v.Errors()["name"].Messages(), "Name must be blank")
+}
+
+func TestMultipleErrorsInOneField(t *testing.T) {
+	v := IsString("", "email").Not().Blank().AnEmail()
+
+	assert.Len(t, v.Errors(), 1)
+	assert.False(t, v.Valid())
+	assert.Len(t, v.Errors()["email"].Messages(), 2)
+	assert.Contains(t, v.Errors()["email"].Messages(), "Email can't be blank")
+	assert.Contains(t, v.Errors()["email"].Messages(), "Email is not an email address")
+}
+
+func TestErrorMarshallJSON(t *testing.T) {
+	v := IsString("", "email").Not().Blank().AnEmail().
+		IsString("", "name").Not().Blank()
+
+	jsonByte, err := json.Marshal(v.Error())
+	assert.NoError(t, err)
+
+	jsonMap := map[string]interface{}{}
+	err = json.Unmarshal(jsonByte, &jsonMap)
+	assert.NoError(t, err)
+
+	assert.Equal(t, jsonMap["name"], "Name can't be blank")
+	emailErrors := jsonMap["email"].([]interface{})
+	assert.Contains(t, emailErrors, "Email can't be blank")
+	assert.Contains(t, emailErrors, "Email is not an email address")
 }
